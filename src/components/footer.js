ad4dd5df87ee1f0b5808c820f9d2f75b47c024bb
MahtabKhan7179 <104351266+MahtabKhan7179@users.noreply.github.com>
2024-10-13 19:22:56 +0530

    Added Footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <div className="bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-b-xl lg:border-0 lg:bg-gray-200 lg:p-0 lg:dark:bg-zinc-800/30 mx-0 max-w-7xl sm:0 lg:0">
+            <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300  bg-gradient-to-b from-zinc-200  backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border-0 lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                Crafted with love&nbsp;
+                <code className="font-mono font-bold">© Mahtab Khan. All rights reserved.</code>
+            </p>
+        </div>
+
+    );
+}
